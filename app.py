c3ce1e9ab5ead7c3edd4ac79996e39b5a9c96b52
Maham Jamil <mahamjameel61@gmail.com>
2026-03-17 01:29:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 # Load environment variables
 load_dotenv()
 # Initialize OpenAI client
-client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 #page_configuration
 st.set_page_config(page_title="AI Chatbot", layout="centered")
@@ -16,21 +16,22 @@ st.title("🤖Simple AI Chatbot")
 st.write("Ask anything!")
 
 # User input
-user_input = st.chat_input("Enter your question:")
+user_input = st.chat_input("Enter your question")
 
 # When user clicks button
-if st.button("Ask AI"):
-
-    if user_input:
-        response = client.responses.create(
-            model="gpt-5-nano",
-            input=user_input,
+ if user_input:
+     st.subheader("Your Question:")
+     st.write(user_input)
+     
+     with st.spinner("Thinking..."):
+         response = client.responses.create(
+             model="gpt-5-nano",
+             input=user_input,
             instructions="you are a helpful assistant that provides concise answers to questions"
-        )
+         )
 
-        # Display result
-        st.subheader("AI Response:")
-        st.write(response.output_text)
+    # Display result
+    st.subheader("AI Response:")
+    st.write(response.output_text)
 
-    else:
-        st.warning("Please enter a question.")
+  
